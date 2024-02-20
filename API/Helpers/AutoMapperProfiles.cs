43feb2a96b05febf48059a8aefb83d6ac378d28a
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-02-20 03:30:28 +0200

    Updating resourses (user etc)

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,4 +1,5 @@
-﻿using API.Entities;
+﻿using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using AutoMapper;
 namespace API;
@@ -10,5 +11,6 @@ public class AutoMapperProfiles:Profile
             .ForMember(dest=>dest.PhotoUrl,opt=>opt.MapFrom(src=>src.Photos.FirstOrDefault(x=>x.IsMain).Url))
             .ForMember(dest=>dest.Age,opt=>opt.MapFrom(src=>src.DateOfBirth.CalculateAge()));
         CreateMap<Photo,PhotoDto>();
+        CreateMap<MemberUpdateDto,AppUser>();
     }    
 }
