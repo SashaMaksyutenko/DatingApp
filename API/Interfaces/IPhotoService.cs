0f302b47843b1c553a1af4a0586c71a6a2d3cade
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-02-22 02:28:15 +0200

    Adding photo upload functionality

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,7 @@
+﻿using CloudinaryDotNet.Actions;
+namespace API;
+public interface IPhotoService
+{
+    Task<ImageUploadResult>AddPhotoAsync(IFormFile file);
+    Task<DeletionResult>DeletePhotoAsync(string publicId);
+}
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -16,8 +16,7 @@ export class AccountService {
       map((response:User)=>{
         const user=response;
         if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -27,13 +26,13 @@ export class AccountService {
       map(response => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
   }
   setCurrentUser(user:User){
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
   logout(){
