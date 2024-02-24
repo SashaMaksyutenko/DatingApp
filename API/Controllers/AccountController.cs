ded8155a0ef2ec4aeb58e6f8d8ef98f488ce32d7
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-02-24 03:56:41 +0200

    Reactive forms

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 namespace API;
@@ -11,28 +12,30 @@ public class AccountController:BaseApiController
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
-    public AccountController(DataContext context,ITokenService tokenService)
+    private readonly IMapper _mapper;
+    public AccountController(DataContext context,ITokenService tokenService,IMapper mapper)
     {
+        _mapper = mapper;
         _tokenService = tokenService;
         _context = context;
     }
     [HttpPost("register")]  // POST: api/account/register
     public async Task<ActionResult<UserDto>>Register(RegisterDto registerDto)
     {
+        var user=_mapper.Map<AppUser>(registerDto);
         if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
         using var hmac=new HMACSHA512(); 
-        var user=new AppUser{
-            UserName=registerDto.Username.ToLower(),
-            PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt=hmac.Key
-        };
+        user.UserName=registerDto.Username.ToLower();
+        user.PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+        user.PasswordSalt=hmac.Key;
+        
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         return new UserDto
         {
             Username=user.UserName,
             Token=_tokenService.CreateToken(user),
-            PhotoUrl=user.Photos.FirstOrDefault(x=>x.IsMain).Url
+            KnownAs=user.KnownAs
         };
     }
     [HttpPost("login")]
@@ -50,7 +53,8 @@ public class AccountController:BaseApiController
         {
             Username=user.UserName,
             Token=_tokenService.CreateToken(user),
-            PhotoUrl=user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
+            PhotoUrl=user.Photos.FirstOrDefault(x=>x.IsMain)?.Url,
+            KnownAs=user.KnownAs
         };
     }
     private async Task<bool>UserExists(string username)
