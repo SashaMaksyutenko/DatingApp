882bcebbad0f1f03ad148b803e6aeab155eb0645
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-03-01 02:02:35 +0200

    Adding the Like User feature

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,9 @@
+﻿using API.Entities;
+using API.Helpers;
+namespace API.Interfaces;
+public interface ILikesRepository
+{
+   Task<UserLike>GetUserLike(int sourceUserId,int targetUserId);
+   Task<AppUser>GetUserWithLikes(int userId);
+   Task<PagedList<LikeDto>>GetUserLikes(LikesParams likesParams);
+}
